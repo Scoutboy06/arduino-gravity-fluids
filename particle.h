610c894a9594ec92a153bb40055eea097fe9627d
Elias Wennerlund <elias06wennerlund@gmail.com>
2024-01-22 17:32:44 +0100

    Edits to Particle class

--- particle.h
+++ particle.h
@@ -1,30 +1,32 @@
 #pragma once
 
+#include "list.h"
 #include "neighbor_manager.h"
 #include "vec.h"
 
 class Particle {
  public:
-  Vec pos;
-  Vec prevPos;
-  Vec vel;
-  Vec acc;
+  Vector2 pos;
+  Vector2 prevPos;
+  Vector2 vel;
+  Vector2 acc;
   float radius;
   unsigned int index;
 
   Particle() {}
-  Particle(float x, float y) {
-    this->pos = Vec(x, y);
-    this->prevPos = Vec(x, y);
-    this->vel = Vec();
-    this->acc = Vec();
+  Particle(float x, float y, float radius) {
+    this->pos = Vector2(x, y);
+    this->prevPos = Vector2(x, y);
+    this->vel = Vector2();
+    this->acc = Vector2();
+    this->radius = radius;
   }
 
   float dist(Particle& other) { return this->pos.dist(other.pos); }
 
   float distSq(Particle& other) { return this->pos.distSq(other.pos); }
 
-  Particle** neighbors() {
+  List<Particle*> neighbors() {
     return NeighborManager::GetInstance().getNeighborsOf(this);
   }
 };
