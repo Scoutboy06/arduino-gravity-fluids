08dc83df7048e929272cf36130201017139f2b19
Elias Wennerlund <elias06wennerlund@gmail.com>
2024-02-21 22:52:51 +0100

    Start of grid class

--- grid.h
+++ grid.h
@@ -1,13 +1,30 @@
 #pragma once
 
+#include "globals.h"
 #include "list.h"
 #include "particle.h"
 #include "particle_manager.h"
 
+#define CELL_SIZE 16
+const uint8_t ROWS = SCREEN_HEIGHT / CELL_SIZE;
+const uint8_t COLS = SCREEN_WIDTH / CELL_SIZE;
+
 class Grid {
  public:
+  Grid() {}
+
   void moveParticle(Particle& p) {
-    // TODO: Implementation
+    int currCellIndex;
+
+    for (int i = 0; i < ROWS * COLS; i++) {
+      for (Particle* par : _grid[i]) {
+        if (p.is(*par)) {
+          currCellIndex = i;
+        }
+      }
+    }
+
+    // int cellRow
   }
 
   List<Particle*> possibleNeighbors(Particle& p) {
@@ -15,4 +32,5 @@ class Grid {
   }
 
  private:
+  List<Particle*> _grid[ROWS * COLS];
 };
