70dbe2d76249b41ba2392708a9a2a21fd68427a4
Elias Wennerlund <elias06wennerlund@gmail.com>
2024-02-21 22:52:05 +0100

    Updating methods

--- particle.h
+++ particle.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "list.h"
-// #include "neighbor_manager.h"
 #include "vec.h"
 
 class Particle {
@@ -9,21 +8,19 @@ class Particle {
   Vector2 pos;
   Vector2 prevPos;
   Vector2 vel;
-  uint32_t index;
+  uint16_t index;
 
   Particle() {}
-  Particle(float x, float y, uint32_t index) {
+  Particle(float x, float y, uint16_t index) {
     this->pos = Vector2(x, y);
     this->prevPos = Vector2(x, y);
     this->vel = Vector2();
     this->index = index;
   }
 
-  float dist(Particle& other) { return this->pos.dist(other.pos); }
+  float dist(const Particle& other) { return this->pos.dist(other.pos); }
 
-  float distSq(Particle& other) { return this->pos.distSq(other.pos); }
+  float distSq(const Particle& other) { return this->pos.distSq(other.pos); }
 
-  // List<Particle*> neighbors() {
-  //   return NeighborManager::GetInstance().getNeighborsOf(this);
-  // }
+  bool is(const Particle& other) { return this == &other; }
 };
